54c3ac18c408722859383d2946dce1daf4407d2f
Andres Pacheco <apacheco73@hotmail.com>
2020-04-25 16:59:41 -0300

    Updates to root module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,15 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { PlayerModule } from './player/player.module';
+import { TeamModule } from './team/team.module';
+import { MatchModule } from './match/match.module';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(), PlayerModule],
+  imports: [
+    TypeOrmModule.forRoot(),
+    PlayerModule,
+    TeamModule,
+    MatchModule
+  ],
 })
 export class AppModule {}
