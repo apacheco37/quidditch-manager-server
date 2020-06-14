c1ce49d18a4cb08b6f6aaffcbd8090d66705914a
Andres Pacheco <apacheco73@hotmail.com>
2020-06-14 13:15:28 -0300

    Updates to match module

--- src/match/entities/match.order.entity.ts
+++ src/match/entities/match.order.entity.ts
@@ -1,4 +1,4 @@
-import { Entity, PrimaryGeneratedColumn, Column } from "typeorm";
+import { Entity, PrimaryGeneratedColumn, Column, OneToMany, OneToOne, JoinColumn } from "typeorm";
 import { Player } from "src/player/entities/player.entity";
 
 @Entity()
@@ -6,12 +6,13 @@ export class MatchOrder {
   @PrimaryGeneratedColumn()
   Id: number;
 
-  @Column()
+  @OneToMany(type => Player, player => player.Id)
   Chasers: Player[];
 
-  @Column()
+  @OneToMany(type => Player, player => player.Id)
   Beaters: Player[];
 
-  @Column()
+  @OneToOne(type => Player)
+  @JoinColumn()
   Keeper: Player;
 }
--- src/match/match.module.ts
+++ src/match/match.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { MatchController } from './match.controller';
 import { MatchService } from './match.service';
+import { Match } from './entities/match.entity';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Match])],
   controllers: [MatchController],
   providers: [MatchService]
 })
