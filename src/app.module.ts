6f706db15b5ef8ee6e6540e9429d15f26fd83d62
APacheco37 <apacheco73@hotmail.com>
2021-06-09 09:01:06 -0300

    Initial auth work - implemented passport-local strategy

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,13 +4,17 @@ import { PlayerModule } from './player/player.module';
 import { TeamModule } from './team/team.module';
 import { MatchModule } from './match/match.module';
 import { typeOrmConfig } from 'ormconfig';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [
     TypeOrmModule.forRoot(typeOrmConfig),
     PlayerModule,
     TeamModule,
-    MatchModule
+    MatchModule,
+    AuthModule,
+    UsersModule
   ],
 })
 export class AppModule {}
