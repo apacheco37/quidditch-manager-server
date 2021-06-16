bd89d52bb2c37abe07167b3828aabac822547763
APacheco37 <apacheco73@hotmail.com>
2021-06-16 20:01:22 -0300

    Fix existing unit tests

--- src/auth/auth.service.spec.ts
+++ src/auth/auth.service.spec.ts
@@ -1,12 +1,25 @@
+import { JwtService } from '@nestjs/jwt';
 import { Test, TestingModule } from '@nestjs/testing';
+import { UsersService } from '../users/users.service';
 import { AuthService } from './auth.service';
+
+const mockedJwtService = {
+  sign: () => ''
+}
 
 describe('AuthService', () => {
   let service: AuthService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [AuthService],
+      providers: [
+        UsersService,
+        AuthService,
+        {
+          provide: JwtService,
+          useValue: mockedJwtService
+        }
+      ],
     }).compile();
 
     service = module.get<AuthService>(AuthService);
